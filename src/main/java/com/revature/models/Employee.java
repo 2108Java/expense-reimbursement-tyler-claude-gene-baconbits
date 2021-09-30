60c3da797eaaa9d52a51df0d4f75b39ea7b2efc7
Tyler Nickl <tyler.a.nickl@gmail.com>
2021-09-30 01:05:03 -0700

    Wrote classes for Models package, including Ticket, Employee, and Enums for TicketStatus and RequestType. Added empty RequestHandler and Controller to Service package.

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -1,5 +1,169 @@
 package com.revature.models;
 
+import java.util.Objects;
+
 public class Employee {
 
+	
+	//FIELDS
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String email;
+	private boolean isManager;
+	
+	
+	
+	
+	//CONSTRUCTORS
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+	public Employee(int empId, String firstName, String lastName, String userName, String password, String email,
+			boolean isManager) {
+		super();
+		this.empId = empId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+		this.isManager = isManager;
+	}
+
+
+
+	//METHODS
+	public int getEmpId() {
+		return empId;
+	}
+
+
+
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+
+	public boolean isManager() {
+		return isManager;
+	}
+
+
+
+
+	public void setManager(boolean isManager) {
+		this.isManager = isManager;
+	}
+
+
+
+	//FOR HASHING PASSWORD
+	@Override
+	public int hashCode() {
+		return Objects.hash(password);
+	}
+
+
+	
+
+	//FOR HASHING PASSWORD
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(password, other.password);
+	}
+	
+	
+
+	
+	
+	
+	
 }
--- src/main/java/com/revature/models/Ticket.java
+++ src/main/java/com/revature/models/Ticket.java
@@ -1,12 +1,142 @@
 package com.revature.models;
 
+import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
 public class Ticket {
 
+	//FIELDS
 	private int id; //serial, primary key in database
-	private int employeeId; //person who is requesting reimbursement
+	private int employeeId; //person who is requesting reimbursement, foreignkey
 	private double amount;
-	private String type; //lodging, travel, food, or other
 	private String description; //provided by employee on submission
-	private int status; //approved, pending, or rejected
+	private RequestType type; //lodging, travel, food, or other
+	private TicketStatus status; //approved, pending, or rejected
+	private ArrayList<Calendar> ticketHistory; 	//to collect timestamp data from the db for display to user
+												//probably will be separate table in db, 
+												//but can be part of TIcket object
+	
+	
+	//CONSTRUCTORS
+	public Ticket() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Ticket(int id, int employeeId, double amount, RequestType type, String description, TicketStatus status) {
+		super();
+		this.id = id;
+		this.employeeId = employeeId;
+		this.amount = amount;
+		this.type = type;
+		this.description = description;
+		this.status = status;
+	}
+
+
+
+	//METHODS
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+
+
+	public double getAmount() {
+		return amount;
+	}
+
+
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+
+
+	public RequestType getType() {
+		return type;
+	}
+
+
+
+	public void setType(RequestType type) {
+		this.type = type;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public TicketStatus getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(TicketStatus status) {
+		this.status = status;
+	}
+
+
+
+	public ArrayList<Calendar> getTicketHistory() {
+		return ticketHistory;
+	}
+
+
+
+	public void setTicketHistory(ArrayList<Calendar> ticketHistory) {
+		this.ticketHistory = ticketHistory;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Ticket [id=" + id + ", employeeId=" + employeeId + ", amount=" + amount + ", description=" + description
+				+ ", type=" + type + ", status=" + status + "]"; //excludes ticketHistory
+	}
+
+
+
+
+
+	
+	
 	
 }
+
+
