5fbb84837f389435b585144392063486f74b4424
Tyler Nickl <tyler.a.nickl@gmail.com>
2021-10-10 21:12:11 -0700

    adding FakeController class as a test of git after merge, re-clone, re-branch

---
+++ src/main/java/com/revature/controllers/FakeController.java
@@ -0,0 +1,7 @@
+package com.revature.controllers;
+
+public class FakeController {
+
+	//just adding this file as a test to see that my git is set up correctly after the merge, re-clone, re-branch
+	
+}
